b7516175da963cca608dbf3ffd94f442752f19b5
vekim00100 <vekim@ucsc.edu>
2024-02-02 20:45:53 -0800

    main + adding reset

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: RNGolf
+// Name: Vivian Kim
+// Date: 2/2/2024
+
+'use strict'
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 960,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Play ]
+}
+
+let game = new Phaser.Game(config)
+
+let { width, height } = game.config
